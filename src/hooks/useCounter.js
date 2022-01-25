3e619a9195d0d05a1feb0dd2c4d17333cce505f0
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-01-25 10:45:57 -0500

    probar el useCounter

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,19 @@
+import { useCallback, useState } from "react"
+export const useCounter = (initialCount = 0) => {
+  const [ count, setCount ] = useState(initialCount)
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  }, []) 
+  
+  const reset = useCallback(() => {
+    setCount(initialCount);
+  }, [initialCount]);
+  
+
+  return {
+    count,
+
+    increment,
+    reset
+  }
+};
